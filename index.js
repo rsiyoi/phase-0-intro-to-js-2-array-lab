9ea577c6d9b1c85a9fc9d71316cdf9dcd2f648b8
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-04-02 13:14:38 +0300

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+};
+
+function destructivelyPrependCat(name){
+cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(){
+cats.pop();
+    };
+    
+function destructivelyRemoveFirstCat(){
+cats.shift();
+};
+
+function appendCat(cats){
+return cats = ['Milo', 'Otis', 'Garfield',cats]
+};
+
+function prependCat(cats){
+return cats = [cats, 'Milo', 'Otis', 'Garfield']
+};
+
+function removeLastCat() {
+return cats.slice(0, -1)
+};
+
+function removeFirstCat(){
+return cats.slice(1)
+};
